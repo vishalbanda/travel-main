098cebce9964df20652e83d7684f74bc64f5955e
vishal <vishal.banda5@gmail.com>
2023-01-09 10:59:06 +0530

    updated the goacomponent

---
+++ src/app/place/goa/goa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goa',
+  templateUrl: './goa.component.html',
+  styleUrls: ['./goa.component.css']
+})
+export class GoaComponent {
+
+}
